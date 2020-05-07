9c4ba918bebe09dfa598360adda0425c482be8c3
Taylor Keller <takeller17@gmail.com>
2020-05-07 17:37:17 -0600

    Complete user story 8

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,10 @@
 class PetsController < ApplicationController
 
   def index
-    @pets = Pet.all
+    if params[:shelter_id]
+      @pets = Shelter.find(params[:shelter_id]).pets
+    else
+      @pets = Pet.all
+    end 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   patch "/shelters/:id", to: "shelters#update"
   delete "shelters/:id", to: "shelters#destroy"
   get "/pets", to: "pets#index"
+  get "/shelters/:shelter_id/pets", to: "pets#index"
 end
