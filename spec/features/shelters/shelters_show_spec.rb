5ee5e4c7e09796dced67685ee7d9473ecc8264dc
Taylor Keller <takeller17@gmail.com>
2020-05-07 14:11:47 -0600

    Complete user story 7

--- spec/features/shelters/shelters_show_spec.rb
+++ spec/features/shelters/shelters_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Shelters show page", type: :feature do
   it "Can see a single shelter" do
     shelter1 = Shelter.create(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
-    # shelter2 = Shelter.create(name: "Kitty Rescue", address: "1505 Kitty Dr.", city: "Denver", state: "Colorado", zip: "80025")
+
 
     visit "shelters/#{shelter1.id}"
 
@@ -28,5 +28,19 @@ describe "Shelters show page", type: :feature do
     click_on "Update Shelter"
 
     expect(page).to have_content("2000 Puppy Dr.")
+  end
+
+  it "can delete a shelter" do
+    shelter1 = Shelter.create(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
+    shelter2 = Shelter.create(name: "Kitty Rescue", address: "1505 Kitty Dr.", city: "Denver", state: "Colorado", zip: "80025")
+
+    visit "shelters/#{shelter1.id}"
+
+    click_on "Delete Shelter"
+
+    expect(current_path).to eq("/shelters")
+
+    expect(page).to have_content(shelter2.name)
+    expect(page).to_not have_content(shelter1.name)
   end
 end
