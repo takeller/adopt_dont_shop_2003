5e903cfa7a97fc986ad8328c4a436fa69ad43871
Taylor Keller <takeller17@gmail.com>
2020-05-08 16:55:11 -0600

    complete user story 16

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -26,11 +26,28 @@ describe "Pets Index Page" do
       shelter1 = Shelter.create(name: "Puppies United")
       pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", shelter_id: shelter1.id)
 
-      visit "/pets" 
+      visit "/pets"
 
       click_on "Update #{pet1.name}"
 
       expect(current_path).to eq("/pets/#{pet1.id}/edit")
+    end
+
+    it "can delete a pet" do
+      shelter1 = Shelter.create(name: "Puppies United")
+      shelter2 = Shelter.create(name: "Kittens R Us")
+      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", shelter_id: shelter1.id)
+      pet2 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Bob", approximate_age: "1", sex: "Male", shelter_id: shelter1.id)
+      pet3 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Lars", approximate_age: "14", sex: "Male", shelter_id: shelter2.id)
+
+      visit "/pets"
+
+      click_on "Delete #{pet1.name}"
+
+      expect(current_path).to eq("/pets")
+      expect(page).to have_content(pet2.name)
+      expect(page).to have_content(pet3.name)
+      expect(page).to_not have_content(pet1.name)
     end
   end
 end
